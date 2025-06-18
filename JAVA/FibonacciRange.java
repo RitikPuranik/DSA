414d536c547a364ec509c932dc42a1ab1d8f77fb
Ritik Puranik <ritikpuranik28@gmail.com>
2025-06-18 23:11:01 +0530

    hw

--- JAVA/FibonacciRange.java
+++ JAVA/FibonacciRange.java
@@ -2,6 +2,12 @@ package JAVA;
 import java.util.Scanner;
 
 public class FibonacciRange {
+    static int fib(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the range: ");
@@ -9,5 +15,9 @@ public class FibonacciRange {
         int start = scanner.nextInt();
         System.out.print("Enter the ending number: ");
         int end = scanner.nextInt();
+        for(int i=start+1;i<=end+1;i++){
+            System.out.print(fib(i)+" ");
+        }
+        scanner.close();
     }
 }
