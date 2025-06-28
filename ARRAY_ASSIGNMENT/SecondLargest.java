78b0fce6d0f65d4d27d06ba239be5a2a06e19ec4
Ritik Puranik <ritikpuranik28@gmail.com>
2025-06-28 21:48:01 +0530

    dsa

--- ARRAY_ASSIGNMENT/SecondLargest.java
+++ ARRAY_ASSIGNMENT/SecondLargest.java
@@ -2,6 +2,32 @@ package ARRAY_ASSIGNMENT;
 import java.util.Scanner;
 
 public class SecondLargest {
+    // public static void main(String[] args) {
+    //     Scanner sc= new Scanner(System.in);
+    //     System.out.println("enter the  size of array");
+    //     int n= sc.nextInt();
+    //     int arr[]=new int[n];
+    //     System.out.println("enter the elements of array");
+    //     for(int i=0;i<n;i++){
+    //         arr[i]=sc.nextInt();
+    //     }
+    //     int max=Integer.MIN_VALUE;
+    //     for(int i=0;i<n;i++){
+    //         if(arr[i]>max){
+    //             max=arr[i];
+    //         }
+    //     }
+    //     int secondMax=Integer.MIN_VALUE;
+    //     for(int i=0;i<n;i++){
+    //         if(arr[i]>secondMax && arr[i]<max){
+    //             secondMax=arr[i];
+    //         }
+    //     }
+    //     System.out.println("second largest element is "+secondMax);
+    //     sc.close();
+    // }
+
+
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("enter the  size of array");
@@ -12,18 +38,17 @@ public class SecondLargest {
             arr[i]=sc.nextInt();
         }
         int max=Integer.MIN_VALUE;
+        int Secondmax=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
             if(arr[i]>max){
+                Secondmax=max;
                 max=arr[i];
             }
-        }
-        int secondMax=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            if(arr[i]>secondMax && arr[i]<max){
-                secondMax=arr[i];
+            else if(arr[i]>Secondmax && arr[i]!=max){
+                Secondmax=arr[i];
             }
         }
-        System.out.println("second largest element is "+secondMax);
+        System.out.println(Secondmax);
         sc.close();
     }
 }
--- ARRAY_ASSIGNMENT/SecondSmallest.java
+++ ARRAY_ASSIGNMENT/SecondSmallest.java
@@ -1,6 +1,34 @@
 package ARRAY_ASSIGNMENT;
 import java.util.Scanner;
 public class SecondSmallest {
+    // public static void main(String[] args) {
+    //     Scanner sc= new Scanner(System.in);
+    //     System.out.println("enter the  size of array");
+    //     int n= sc.nextInt();
+    //     int arr[]=new int[n];
+    //     System.out.println("enter the elements of array");
+    //     for(int i=0;i<n;i++){
+    //         arr[i]=sc.nextInt();
+    //     }
+    //     int min=Integer.MAX_VALUE;
+    //     for(int i=0;i<n;i++){
+    //         if(arr[i]<min){
+    //             min=arr[i];
+    //         }
+    //     }
+    //     int secondMin=Integer.MAX_VALUE;
+    //     for(int i=0;i<n;i++){
+    //         if(arr[i]<secondMin && arr[i]>min){
+    //             secondMin=arr[i];
+    //         }
+    //     }
+    //     System.out.println("second smallest element is "+secondMin);
+    //     sc.close();
+    // }
+
+
+
+
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("enter the  size of array");
@@ -11,18 +39,17 @@ public class SecondSmallest {
             arr[i]=sc.nextInt();
         }
         int min=Integer.MAX_VALUE;
+        int SecondSmallest=Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
             if(arr[i]<min){
+                SecondSmallest=min;
                 min=arr[i];
             }
-        }
-        int secondMin=Integer.MAX_VALUE;
-        for(int i=0;i<n;i++){
-            if(arr[i]<secondMin && arr[i]>min){
-                secondMin=arr[i];
+            else if(arr[i]<SecondSmallest && arr[i]>min){
+                SecondSmallest=arr[i];
             }
         }
-        System.out.println("second smallest element is "+secondMin);
+        System.out.println("second smallest element is "+SecondSmallest);
         sc.close();
     }
 }
