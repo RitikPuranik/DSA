97a5f64037d46ac0d5deb43b75ffc7ac4d3abba4
Ritik Puranik <ritikpuranik28@gmail.com>
2025-07-10 03:14:29 +0530

    stack

---
+++ DemoArrayList.java
@@ -0,0 +1,14 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.RandomAccess;
+public class DemoArrayList {
+    public static void main(String[] args) {
+
+        ArrayList al = new ArrayList();
+    
+        System.out.println(al instanceof Cloneable);
+        System.out.println(al instanceof Serializable);
+        System.out.println(al instanceof RandomAccess);
+
+    }
+}
---
+++ stack/StackExample.java
@@ -0,0 +1,21 @@
+
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        System.out.println(s.empty());
+
+        s.push("A");
+        s.push("B");
+        s.push("C");
+
+        System.out.println(s.empty());
+        System.out.println(s);
+        s.pop();
+
+        System.out.println(s);
+        System.out.println(s.peek());
+
+    }
+}
